1ac0cbe19f44e2c877e36f1829307e4d5435f0d9
Bui Vu Thien An <nhok.kulun56@gmail.com>
2023-07-29 23:29:47 +0700

    Update change

--- src/main/java/com/example/demo/controller/ProductController.java
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.exception.InvalidRequestParameterException;
@@ -24,15 +25,20 @@ public class ProductController {
 	ProductService productService;
 
 	@GetMapping("/products")
-	public ResponseEntity<?> getAllProduct() {
+	public ResponseEntity<?> getAll() {
 		return ResponseEntity.ok(productService.findAll());
 	}
-	
+
+	@GetMapping("/search")
+	public ResponseEntity<?> findByProductName(@RequestParam("name") String name) {
+		return ResponseEntity.ok(productService.findByName(name));
+	}
+
 	@GetMapping("/product/{id}")
 	public ResponseEntity<?> findById(@PathVariable("id") Integer id) throws InvalidRequestParameterException {
 		return ResponseEntity.ok(productService.findById(id));
 	}
-	
+
 	@PostMapping("/product/filter")
 	public ResponseEntity<?> filterProduct(@RequestBody Optional<ProductFilter> data) {
 		return ResponseEntity.ok(productService.filterProduct(data.get()));
