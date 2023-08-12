00f3da8f02d8ba3fe37c2a686e5c463a27146579
Thanh Sơn <thanhson9112@gmail.com>
2023-08-12 21:25:28 +0700

    api user

--- src/main/java/com/example/demo/repository/UsersRepository.java
+++ src/main/java/com/example/demo/repository/UsersRepository.java
@@ -6,8 +6,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.entity.Users;
+
 @Repository
-public interface UsersRepository extends JpaRepository<Users, Integer>{
+public interface UsersRepository extends JpaRepository<Users, Integer> {
 	Optional<Users> findByEmailAndPassword(String email, String password);
 	Optional<Users> findByEmail(String email);
+	Optional<Users> findByIsAdmin(Boolean isAdmin);
+	Users findByFullname(String name);
 }
