e4ecb277bfdcfe845a41b90036cacbbf2543b4a0
khaiminh0401 <85891822+khaiminh0401@users.noreply.github.com>
2023-08-15 01:57:45 +0700

    add /api/order/[id]

--- src/main/java/com/example/demo/controller/OrderController.java
+++ src/main/java/com/example/demo/controller/OrderController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,6 +28,10 @@ public class OrderController {
     @GetMapping("")
     public ResponseEntity<?> getAll (@RequestParam("userId") Optional<Integer> userId){ 
         return ResponseEntity.ok(userId.isPresent() ?orderSerivce.findByCustomerId(userId.get()): orderSerivce.findAll());
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getById(@PathVariable("id") Integer id){
+        return ResponseEntity.ok(orderSerivce.findById(id));
     }
 
     @PostMapping("/save")
