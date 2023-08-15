373bbc779716f9a5d12b5651abc8f3b2ea9ffc14
nguyensontrungkien <nguyentrungkien08010801@gmail.com>
2023-08-15 07:02:36 +0700

    status orders

--- src/main/java/com/example/demo/controller/OrderController.java
+++ src/main/java/com/example/demo/controller/OrderController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -29,11 +30,22 @@ public class OrderController {
     public ResponseEntity<?> getAll (@RequestParam("userId") Optional<Integer> userId){ 
         return ResponseEntity.ok(userId.isPresent() ?orderSerivce.findByCustomerId(userId.get()): orderSerivce.findAll());
     }
+    
+    @GetMapping("/fillOrdersByStatus")
+    public ResponseEntity<?> getDhByStatusUser (@RequestParam("userId") Integer userId,@RequestParam("status") Integer status){ 
+        return ResponseEntity.ok(orderSerivce.findDhByStatusUser(userId, status));
+    }
+    
     @GetMapping("/{id}")
     public ResponseEntity<?> getById(@PathVariable("id") Integer id){
         return ResponseEntity.ok(orderSerivce.findById(id));
     }
 
+    @PutMapping("/update")
+    public ResponseEntity<?> update (@RequestBody Orders order) throws MessagingException{
+        return ResponseEntity.ok(orderSerivce.update(order));
+    }
+    
     @PostMapping("/save")
     public ResponseEntity<?> save (@RequestBody Orders order) throws MessagingException{
         return ResponseEntity.ok(orderSerivce.save(order));
